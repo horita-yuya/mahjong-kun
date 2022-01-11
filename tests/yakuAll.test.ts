f0a10b5710565be2663cebdc30c54c13731a525a
horita-yuya <horitayuya@gmail.com>
2022-01-12 01:22:48 +0900

    Refactor

--- tests/yakuAll.test.ts
+++ tests/yakuAll.test.ts
@@ -1,7 +1,6 @@
 import { YakuAll } from "../types/yaku";
 import { Equal, ExpectTrue } from "../types/utilType";
 import { HanValue } from "../types/han";
-import { HuValue } from "../types/hu";
 
 type Case1 = YakuAll<["o2", "o3", "o4", "I6", "I7", "I8", "C4", "C5", "C6", "I6", "I7", "I8", "o4", "o4"], "o4">
 type ExpectYaku1 = ExpectTrue<Equal<Case1, ["pinhu", "tanyao", "zero"]>>
