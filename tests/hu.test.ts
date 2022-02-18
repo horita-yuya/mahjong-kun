13aaa69b440531f283e8fb391972462faa1768a8
horita-yuya <horitayuya@gmail.com>
2022-02-18 18:34:44 +0900

    Round up hu value

--- tests/hu.test.ts
+++ tests/hu.test.ts
@@ -2,5 +2,8 @@ import { Equal, HuValue } from "../types";
 import { ExpectTrue } from "./testUtil";
 
 type Case1 = ExpectTrue<Equal<HuValue<["hu20"]>, 20>>
-type Case2 = ExpectTrue<Equal<HuValue<["hu20", "hu10", "hu2"]>, 32>>
+type Case2 = ExpectTrue<Equal<HuValue<["hu20", "hu10", "hu2"]>, 40>>
 type Case3 = ExpectTrue<Equal<HuValue<["hu20", "hu10"]>, 30>>
+type Case4 = ExpectTrue<Equal<HuValue<["hu20", "hu16"]>, 40>>
+type Case5 = ExpectTrue<Equal<HuValue<["hu20", "hu16", "hu8"]>, 50>>
+type Case6 = ExpectTrue<Equal<HuValue<["hu32", "hu2"]>, 40>>
--- types/hu.ts
+++ types/hu.ts
@@ -42,4 +42,22 @@ type HuValues = {
   ],
 }
 
-export type HuValue<ALL extends Hu[]> = HuCalculate<ALL>["length"]
+type Even = 2 | 4 | 6 | 8;
+type Number = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
+type NextNumber = {
+  "1": 20,
+  "2": 30,
+  "3": 40,
+  "4": 50,
+  "5": 60,
+  "6": 70,
+  "7": 80,
+  "8": 90,
+  "9": 100,
+  "10": 110,
+  "11": 120,
+}
+
+type Round<T extends number> = `${T}` extends `${infer X}${Even}` ? X extends `${Number}` ? NextNumber[X] : never : T;
+
+export type HuValue<ALL extends Hu[]> = Round<HuCalculate<ALL>["length"]>
