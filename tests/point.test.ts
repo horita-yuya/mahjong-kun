ac5d6acf3a22df05384065689a08f506cc8873b9
horita-yuya <horitayuya@gmail.com>
2022-02-26 15:56:29 +0900

    Update test

--- tests/point.test.ts
+++ tests/point.test.ts
@@ -1,11 +1,16 @@
 import { Calculate, Equal } from "../types";
 import { ExpectTrue } from "./testUtil";
 
-type RonChildCase1 = ExpectTrue<Equal<Calculate<1, 30, "ron", "child", null>, "1000">>;
-// ピンヅモは20符として計算される
+type RonChildCase1_1 = ExpectTrue<Equal<Calculate<1, 30, "ron", "child", null>, "1000">>;
+type RonChildCase1_2 = ExpectTrue<Equal<Calculate<2, 30, "tumo", "child", null>, "500,1000">>;
+type RonChildCase1_3 = ExpectTrue<Equal<Calculate<2, 40, "tumo", "child", null>, "700,1300">>;
+// ピンヅモは20符として計算
 type RonChildCase2_1 = ExpectTrue<Equal<Calculate<2, 20, "tumo", "child", "pin-tumo">, "400,700">>;
 type RonChildCase2_2 = ExpectTrue<Equal<Calculate<2, 30, "tumo", "child", "pin-tumo">, "400,700">>;
 type RonChildCase2_3 = ExpectTrue<Equal<Calculate<2, 40, "tumo", "child", "pin-tumo">, "400,700">>;
 // 七対子は25符として計算
 type RonChildCase3_1 = ExpectTrue<Equal<Calculate<3, 30, "tumo", "child", "chitoitsu">, "800,1600">>;
 type RonChildCase3_2 = ExpectTrue<Equal<Calculate<3, 25, "tumo", "child", "chitoitsu">, "800,1600">>;
+// ピンヅモ以外で20符になった場合は30符で計算
+type RonChildCase4_1 = ExpectTrue<Equal<Calculate<1, 20, "tumo", "child", null>, "300,500">>;
+type RonChildCase4_2 = ExpectTrue<Equal<Calculate<1, 20, "ron", "child", null>, "1000">>;
