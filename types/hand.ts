b8671da7cf057284dcc2d4c70ed8218b92f1afc6
horita-yuya <horitayuya@gmail.com>
2022-02-26 22:11:40 +0900

    Update

--- types/hand.ts
+++ types/hand.ts
@@ -1,15 +1,9 @@
 import { Tile } from "./tile";
 
-export type Hand = [
-  Tile, Tile, Tile,
-  Tile, Tile, Tile,
-  Tile, Tile, Tile,
-  Tile, Tile, Tile,
-  Tile, Tile
-]
+export type Hand = [Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile];
 
-export type Chunk1<HAND extends Hand> = [HAND[0], HAND[1], HAND[2]]
-export type Chunk2<HAND extends Hand> = [HAND[3], HAND[4], HAND[5]]
-export type Chunk3<HAND extends Hand> = [HAND[6], HAND[7], HAND[8]]
-export type Chunk4<HAND extends Hand> = [HAND[9], HAND[10], HAND[11]]
-export type Pair<HAND extends Hand> = [HAND[12], HAND[13]]
+export type Chunk1<HAND extends Hand> = [HAND[0], HAND[1], HAND[2]];
+export type Chunk2<HAND extends Hand> = [HAND[3], HAND[4], HAND[5]];
+export type Chunk3<HAND extends Hand> = [HAND[6], HAND[7], HAND[8]];
+export type Chunk4<HAND extends Hand> = [HAND[9], HAND[10], HAND[11]];
+export type Pair<HAND extends Hand> = [HAND[12], HAND[13]];
